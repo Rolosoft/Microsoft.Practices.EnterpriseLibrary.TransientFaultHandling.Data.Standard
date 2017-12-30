b653eccb60e362662f1518d570948b44dd7e09d6
Rowland O'Connor <rowland.oconnor@emailhippo.com>
2017-12-30 23:38:55 +0000

    Initial commit

---
+++ src/Components/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿#region license
+// ==============================================================================
+// Microsoft patterns & practices Enterprise Library
+// Transient Fault Handling Application Block
+// ==============================================================================
+// Copyright © Microsoft Corporation.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+// ==============================================================================
+#endregion
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: ComVisible(false)]
+
+[assembly: SecurityTransparent]
+[assembly: NeutralResourcesLanguage("en-US")]
